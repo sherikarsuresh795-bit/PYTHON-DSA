f54ba7c9736c90f1ad7931865ddaf3c86c6afc0e
SURESH SHERIKAR <sherikarsuresh795@gmail.com>
2026-09-07 15:53:03 +0530

    class with multiple objects

---
+++ class with multiple objects.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 15:33:33 2026
+
+@author: NARESH
+"""
+
+class Demo():
+    def addtion(self,a,b):
+        print(a+b)
+    def sub(self,a,b):
+        print(a-b)
+    def mul(self,a,b):
+        print(a*b)
+s1=Demo()
+s2=Demo()
+s3=Demo()
+s1.addtion(10,10)
+s2.sub(10,10)
+s3.mul(10,10)
