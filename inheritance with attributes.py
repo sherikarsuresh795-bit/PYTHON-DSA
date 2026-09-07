6ecf338d61f245d105e99a2cf27c7fbd77b209eb
SURESH SHERIKAR <sherikarsuresh795@gmail.com>
2026-09-07 16:14:55 +0530

    inheritance with attributes

---
+++ inheritance with attributes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 16:10:37 2026
+
+@author: NARESH
+"""
+
+class student:
+    name="kprit"
+    age=21
+class Demo(student):
+    branch="aiml"
+d1=Demo()
+print(d1.name)
+print(d1.branch)
