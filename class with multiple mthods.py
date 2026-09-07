4bf48d936f676622b8e7f3c43802d7f010c8efa0
SURESH SHERIKAR <sherikarsuresh795@gmail.com>
2026-09-07 15:51:57 +0530

    class with multiple methods

---
+++ class with multiple mthods.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 15:13:59 2026
+
+@author: NARESH
+"""
+
+class college:
+    name="kprit"
+    place="ghatkesar"
+    def read(self):
+        age=10
+        print(age)
+    def add(self,a,b):
+        print(a+b)
+s1=college()
+print(s1.name)
+print(s1.place)       
+s1.read()
+s1.add(1,2)
