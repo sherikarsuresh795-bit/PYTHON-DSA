b1535c20c047ee32b43eb0b6175f51bd0a0705f7
SURESH SHERIKAR <sherikarsuresh795@gmail.com>
2026-09-07 15:36:38 +0530

    create a class and object

---
+++ creationofclassobject.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 14:57:09 2026
+
+@author: NARESH
+"""
+
+class student:
+    name="suresh"
+    age=14
+    def read(self):
+        age=30
+        print(age)
+        print("reading")
+o1=student()
+print(o1.age)
+print(o1.name)
+o1.read()
